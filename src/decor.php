43dba4f8be16477f16c4cde901bf6d39b56cc57e
acj <gitlab@acjs.net>
2019-10-22 15:51:41 +0200

    Rectangle PoC.

--- src/decor.php
+++ src/decor.php
@@ -6,10 +6,20 @@ class Decor
         $this->im = new Imagick();
         $this->im->newImage(300, 300, new ImagickPixel('#ffffff'));
         $this->im->setImageFormat('png');
+
+        $this->draw();
     }
 
     public function __toString() {
         return $this->im->getImageBlob();
+    }
+
+    public function draw() {
+        $this->draw = new ImagickDraw();
+        $this->draw->setFillColor('#00ff00');
+        $this->draw->rectangle(20, 20, 40, 40);
+
+        $this->im->drawImage($this->draw);
     }
 }
 
