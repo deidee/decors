092af983b5c8ec70e6511fb24a94d1844080e6fb
acj <gitlab@acjs.net>
2019-10-23 13:23:03 +0200

    Predefined sets.

---
+++ src/facebook-event-cover.jpg.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'class.decor.php';
+
+$im = new Decor(['height' => 628, 'width' => 1200]);
+
+echo $im;
---
+++ src/twitter-cover.jpg.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'class.decor.php';
+
+$im = new Decor(['height' => 1000, 'width' => 3000]);
+
+echo $im;
